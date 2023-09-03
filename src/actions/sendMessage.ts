2b83e0051421cc02643c0d817efefaa24e9c5100
Bharat AI <bharatchhabra@cofactory.ai>
2023-09-03 10:54:57 +0530

    Change endpoint for local

--- src/actions/sendMessage.ts
+++ src/actions/sendMessage.ts
@@ -2,19 +2,22 @@ import axios from "axios"
 import { IMessage } from "../types/message"
 
 export function sendMessage(url: string, messages: IMessage[], state: {}, botId: number, devMode: string){
-    let payload:any = {
-        botId: btoa(botId.toString()),
-        botData: {
-            message_history: messages,
-            state: state
-        }
-    }
+    
     if(devMode === 'local'){
-        payload = {
+        const payload = {
             data: {
                 message_history: messages,
                 state: state
             }
+        }
+        return axios.post(url, payload)
+    }
+
+    const payload:any = {
+        botId: btoa(botId.toString()),
+        botData: {
+            message_history: messages,
+            state: state
         }
     }
     return axios.post(`${url}/sendMessage`, payload)
