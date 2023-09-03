4baec1ce32e86005794d351b29f84f446655564d
pankaj verma <pankaj@avatarlife.com>
2023-09-04 04:25:01 +0530

    revert back getAPIURL method

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,15 @@
-export function getAPIURL(){
-    let url = process.env.REACT_APP_API_URL || 'http://localhost:9001/';
-    let devMode = 'prod'
-    return {
-        url,
-        devMode
+export function getAPIURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const apiParam = urlParams.get('API_URL');
+    let url = process.env.REACT_APP_API_URL || 'http://localhost:9001';
+    let devMode = 'prod';
+    if (apiParam) {
+      url = apiParam;
+      devMode = 'local';
     }
-}
+  
+    return {
+      url,
+      devMode,
+    };
+  }
